0fc95c17fb57a11fb5fb71a42ded30e542c2cf16
Saurav Shandilya <85822926+Saurav-Shandilya@users.noreply.github.com>
2026-04-02 20:14:48 +0530

    Fix target date for hackathon start time

--- src/components/TimeCounter.jsx
+++ src/components/TimeCounter.jsx
@@ -22,7 +22,7 @@ const TimeCounter = () => {
     const now = new Date();
 
     // Hackathon Start Date → 9 April 2026, 10:30 AM
-    const targetDate = new Date(2026, 3, 9, 10, 30, 0); // April = 3
+    const targetDate = new Date(2026, 3, 10, 10, 30, 0); // April = 3
 
     const diff = targetDate - now;
 
